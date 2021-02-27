98b0cf770fb5cd12151acd0f58db88ee98510bfa
fulavu <fulavu@frsystux.fr>
2021-02-27 22:46:00 +0100

    prettier

--- uptime.sh
+++ uptime.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Uptime of the system
-up=`uptime -p`
+up=`uptime -p | tr -d up`
 nbr_users=`uptime | awk -F "," '{ print $2 }'`
 
 
- echo "The system has been up since ${up}"
+ echo "The system has been up since: ${up}"
  echo -e "Number of users currently logged in: ${nbr_users}"
