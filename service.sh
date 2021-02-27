a4bcec56baccf3e2e854988110293998f216cb1b
fulavu <fulavu@frsystux.fr>
2021-02-27 22:58:51 +0100

    parameter verif

--- service.sh
+++ service.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-
-service=$1
-service_state=`systemctl list-units --type=service | grep $service | awk '{print $4}'`
-
+if [ -z $1 ]
+then
+    echo "no parameter provided"
+    echo -e "usage: service.sh <service_name>"
+    exit 1
+else
+    service=$1
+    service_state=`systemctl list-units --type=service | grep $service | awk '{print $4}'`
+    echo "The service ${service} has the state: ${service_state}"
+    exit 0
+fi
