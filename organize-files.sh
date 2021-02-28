0a5ade899194d3afed53fcbce0efb881dc069d3e
fulavu <fulavu@frsystux.fr>
2021-02-28 14:22:19 +0100

    supp echo

--- organize-files.sh
+++ organize-files.sh
@@ -11,23 +11,23 @@ document_files="txt,ppt,doc,docx,csv,xls,xlsx,pdf"
 
 for i in $audio_files
 do 
-    echo "mv $source_folder/*.$i  $destination_folder/Music"
+    mv $source_folder/*.$i  $destination_folder/Music
 done
 
 
 for i in $video_files
 do 
-    echo "mv $source_folder/*.$i  $destination_folder/Videos"
+     mv $source_folder/*.$i  $destination_folder/Videos
 done
 
 
 for i in $image_files
 do 
-    echo "mv $source_folder/*.$i  $destination_folder/Pictures"
+     mv $source_folder/*.$i  $destination_folder/Pictures
 done
 
 
 for i in $document_files
 do 
-     echo "mv $source_folder/*.$i  $destination_folder/Documents"
+    mv $source_folder/*.$i  $destination_folder/Documents
 done
