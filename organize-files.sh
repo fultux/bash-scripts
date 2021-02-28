7a4411144d218fe4146839a81d6a1c591261790d
fulavu <fulavu@frsystux.fr>
2021-02-28 14:19:50 +0100

    folders rearanged

--- organize-files.sh
+++ organize-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source_folder=$1
-destination_folder=$2
+source_folder="/tmp"
+destination_folder="/home/$USER"
 
 audio_files="mp3 ogg wav flac aac"
 video_files="avi,mp4,wmv,mkv"
@@ -11,26 +11,23 @@ document_files="txt,ppt,doc,docx,csv,xls,xlsx,pdf"
 
 for i in $audio_files
 do 
-    echo "mv $source_folder/*.$i  $destination_folder"
-
+    echo "mv $source_folder/*.$i  $destination_folder/Music"
 done
 
 
 for i in $video_files
 do 
-    echo "mv $source_folder/*.$i  $destination_folder"
-
+    echo "mv $source_folder/*.$i  $destination_folder/Videos"
 done
 
 
 for i in $image_files
 do 
-    echo "mv $source_folder/*.$i  $destination_folder"
-
+    echo "mv $source_folder/*.$i  $destination_folder/Pictures"
 done
 
 
 for i in $document_files
 do 
-     echo "mv $source_folder/*.$i  $destination_folder"
+     echo "mv $source_folder/*.$i  $destination_folder/Documents"
 done
