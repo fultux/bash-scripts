e8c4248aef8b53aa0092fa71a04b81b7306b65ed
fulavu <fulavu@frsystux.fr>
2021-02-28 11:38:09 +0100

    echo $i

--- filesystem-space.sh
+++ filesystem-space.sh
@@ -5,8 +5,8 @@ liste_fs_size=`df -h -x devtmpfs -x tmpfs | grep -v Filesystem | awk '{print $1"
 
 for i in $liste_fs_size
 do
-    fs_name=`$i | awk -F "," '{print $1}'`
-    fs_usage=`$i | awk -F "," '{print $2}'`
+    fs_name=`echo "$i" | awk -F "," '{print $1}'`
+    fs_usage=`echo "$i" | awk -F "," '{print $2}'`
     
     if [ $fs_usage >= 90 ]
     then 
