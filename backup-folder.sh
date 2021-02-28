65232a38f1af1973410b9f76d407fc4319d6d374
fulavu <fulavu@frsystux.fr>
2021-02-28 17:43:05 +0100

    new parameters

--- backup-folder.sh
+++ backup-folder.sh
@@ -2,29 +2,34 @@
 #Backup folder in another destination 
 #Script based on Rsync. It assumes that rsync is already installed
 
-if [ -z $1 ] || [ -z $2 ]
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]
 then
     echo "Mandatory parameter not present"
-    echo -e "Usage $0 <source_folder> <destination_folder> (optional archive)(optional <remote_host>)"
+    echo -e "Usage $0 <source_folder> <destination_folder> <backup_name> (optional archive)(optional <remote_host>)"
     exit 1
 fi
+
+
 source_folder=$1
 destination_folder=$2
+backup_name=$3
+date=`date +%F`
 
-if [ -z $3 ]
+
+if [ -z $4 ]
 then
-    tar -cvf $source_folder.tar $source_folder
-    rsync -az $source_folder/$source_folder.tar $destination_folder/
+    tar -cvf $backup_name_$date.tar $source_folder
+    rsync -az ./$backup_name_$date.tar $destination_folder/
     if [ -f $destination_folder/$source_folder.tar ]
     then
         echo "Archive transfered"
-        rm $source_folder/$source_folder.tar
+        rm ./$backup_name_$date.tar
     else 
         echo "Archive not transfered "
         exit 1
     fi
 else
-    remote_host=$3
+    remote_host=$4
     tar -cvf $source_folder.tar $source_folder
     rsync -az $source_folder/$source_folder.tar $3:$source_folder/
     #Laziness
