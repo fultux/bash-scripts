d10be1eae21b14174f9f529ad4a0ec6e00d1db97
fulavu <fulavu@frsystux.fr>
2021-02-28 17:04:18 +0100

    first commit

---
+++ parse-apache-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+"Enter client IP:"
+read ip
+
+nbr_requests=`grep $ip /var/log/apache2/access.log | wc -l`
+nbr_of_get_req=`grep $ip /var/log/apache2/access.log | grep GET | wc -l`
+nbr_of_post_req=`grep $ip /var/log/apache2/access.log | grep POST | wc -l`
+nbr_of_200=`$ip /var/log/apache2/access.log | grep 200| wc -l`
+nbr_of_not_200=`$ip /var/log/apache2/access.log | grep -v 200| wc -l`
+
+
+echo "Statistics for the client $ip:"
+echo -e "------------------------"
+echo "Number of POST request: $nbr_of_post_req"
+echo "Number of GET request: $nbr_of_get_req"
+echo "Number of request with status 200: $nbr_of_200"
+echo "Number of request other than 200: $nbr_of_not_200"
+echo "Total number of request: $nbr_requests "
