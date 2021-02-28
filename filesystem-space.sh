fe6bc9f85eb4f49121cf2889791b3604f2e1fc50
fulavu <fulavu@frsystux.fr>
2021-02-28 12:02:35 +0100

    Warning case

--- filesystem-space.sh
+++ filesystem-space.sh
@@ -3,6 +3,7 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
 NOCOLOR='\033[0m'
 
 liste_fs_size=`df -h -x devtmpfs -x tmpfs | grep -v Filesystem | awk '{print $1","$5}' | tr -d "%"`
@@ -12,9 +13,11 @@ do
     fs_name=`echo "$i" | awk -F "," '{print $1}'`
     fs_usage=`echo "$i" | awk -F "," '{print $2}'`
     
-    if [ $fs_usage -ge 90 ]
-    then 
-        echo -e "${NOCOLOR}$fs_name: ${RED}KO - more than 90% space used."
+    if [ $fs_usage -ge 95 ]
+    then
+        echo -e "${NOCOLOR}$fs_name: ${RED}KO - more than 95% space used."
+    elif  [ $fs_usage -ge 95 ]
+        echo -e "${NOCOLOR}$fs_name: ${ORANGE}WARNING - more than 90% space used."
     else
         echo -e "${NOCOLOR}$fs_name: ${GREEN}OK"
     fi
