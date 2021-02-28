cc8a2a602c2168f099cc6bf242645d2f036ff2d9
fulavu <fulavu@frsystux.fr>
2021-02-28 14:58:53 +0100

    error msg and colors

--- organize-files.sh
+++ organize-files.sh
@@ -10,26 +10,51 @@ document_files="txt ppt doc docx csv xls xlsx pdf"
 
 
 
-echo "--copie des fichiers audio.."
+echo "-- Audio files copy..."
 for i in $audio_files
 do
-find $source_folder -type f -name "*.$i"  -exec mv -t $destination_folder {} \;
+find $source_folder -type f -name "*.$i"  -exec mv -t $destination_folder/Music {} \;
+    if [ $? -ne 0 ]
+    then
+    echo "\033[0;31m ERROR - Error during the copy of Document files"
+    exit 1
+    fi
 done
+echo "\033[0;32m Adio Files Copy : OK"
 
-echo "--copie des fichiers vidéo.."
+echo "-- Video files copy..."
 for i in $video_files
 do 
-     find $source_folder -type f -name "*.$i" -exec mv -t $destination_folder {} \;
+     find $source_folder -type f -name "*.$i" -exec mv -t $destination_folder/Movies {} \;
+    if [ $? -ne 0 ]
+    then
+    echo "\033[0;31m ERROR - Error during the copy of Document files"
+    exit 1
+    fi
 done
+echo "\033[0;32m Adio Files Copy : OK"
 
-echo "--copie des fichiers images.."
+
+echo "-- Image files copy..."
 for i in $image_files
 do 
-     find $source_folder -type f -name "*.$i" -exec mv -t $destination_folder {} \;
+     find $source_folder -type f -name "*.$i" -exec mv -t $destination_folder/Pictures {} \;
+    if [ $? -ne 0 ]
+    then
+    echo "\033[0;31m ERROR - Error during the copy of Document files"
+    exit 1
+    fi
 done
+echo "\033[0;32m Adio Files Copy : OK"
 
-echo "--copie des fichiers documents.."
+echo "-- Document files copy..."
 for i in $document_files
 do 
-    find $source_folder -type f -name "*.$i" -exec mv -t $destination_folder {} \;
+    find $source_folder -type f -name "*.$i" -exec mv -t $destination_folder/Documents {} \;
+    if [ $? -ne 0 ]
+    then
+    echo "\033[0;31m ERROR - Error during the copy of Document files"
+    exit 1
+    fi
 done
+echo "\033[0;32m Adio Files Copy : OK"
