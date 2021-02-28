37de6e7923c3b7ccfa3e82be685e97f483750f98
fulavu <fulavu@frsystux.fr>
2021-02-28 11:55:40 +0100

    nocolor

--- filesystem-space.sh
+++ filesystem-space.sh
@@ -3,6 +3,8 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
 liste_fs_size=`df -h -x devtmpfs -x tmpfs | grep -v Filesystem | awk '{print $1","$5}' | tr -d "%"`
 
 for i in $liste_fs_size
@@ -12,8 +14,8 @@ do
     
     if [ $fs_usage -ge 90 ]
     then 
-        echo -e "$fs_name: ${RED}KO - more than 90% space used."
+        echo -e "${NOCOLOR}$fs_name: ${RED}KO - more than 90% space used."
     else
-        echo -e "$fs_name: ${GREEN}OK"
+        echo -e "${NOCOLOR}$fs_name: ${GREEN}OK"
     fi
 done
